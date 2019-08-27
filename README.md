65eca0d44a1ed1ef63925d80319e70d4dd45a049
Doug Richardson <doug@rekt.email>
2019-08-27 12:12:36 -0700

    recreate repo to remove git lfs

---
+++ README.md
@@ -0,0 +1,2 @@
+# Houdini Examples
+Random Houdini Examples. These files were authored with Houdini Indie (limited commercial version). Unfortunately I believe that means you cannot open them in Houdini Apprentice or non-indie Houdini Commercial licenses.
---
+++ vex/include/limits.h
@@ -0,0 +1,13 @@
+// Limits for VEX data types
+// http://www.sidefx.com/docs/houdini/vex/lang.html#data-types
+//
+// Assumes floats are IEEE 754, though I've seen no documentation confirming this.
+//
+
+#define INT_MIN -2147483648
+#define INT_MAX 2147483647
+
+#define FLOAT_MIN -340282346638528859811704183484516925440.0
+#define FLOAT_MAX 340282346638528859811704183484516925440.0
+
+
